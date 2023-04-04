3a07435e953bf4d140107a62464b0e3ebb84e763
choi-sus <choi-sus@naver.com>
2023-04-04 12:01:44 +0900

    [Design] : font, colot 추가 - 공통 font, color 추가함

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#26D9FD',
+        textDefault: '#FFFFFF',
+        bgDefault: '#101010',
+        bgPaper: '#1F1F1F',
+      },
+    },
   },
   plugins: [],
 };
