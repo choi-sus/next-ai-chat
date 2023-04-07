6bad641f050a33417a96c1af8b9a888d57fa6abf
choi-sus <choi-sus@naver.com>
2023-04-07 10:14:22 +0900

    [Rename] : 파일 명 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as ElButton } from './ElButon';
+export { default as ElButton } from './ElButton';
 export { default as ElImage } from './ElImage';
 export { default as ElInput } from './ElInput';
