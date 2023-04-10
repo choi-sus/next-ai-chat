a1e1f7e57613c6ad601a5516e087bdb1a3b6d197
choi-sus <choi-sus@naver.com>
2023-04-10 13:27:36 +0900

    [Feat] : svg component router 연결

--- src/layout/modules/constants/svgComponentList.tsx
+++ src/layout/modules/constants/svgComponentList.tsx
@@ -2,11 +2,19 @@ import Back from 'public/images/icon-back.svg';
 import Plus from 'public/images/icon-plus.svg';
 import Logo from 'public/images/logo-white.svg';
 
+import { useNavigation } from '@/hooks';
+import PAGES_HREF from '@/types/PageHref';
+
 const SVG_COMPONENT_LIST: {
   [pathname: string]: React.ReactElement<React.SVGProps<SVGSVGElement>>[];
 } = {
-  '/main': [<Logo key="logo" />, <Plus key="plus" />],
-  '/chat': [<Back key="back" />],
+  '/main': [
+    <Logo key="logo" onClick={() => useNavigation().push(PAGES_HREF.MAIN)} />,
+    <Plus key="plus" />,
+  ],
+  '/chat': [
+    <Back key="back" onClick={() => useNavigation().push(PAGES_HREF.MAIN)} />,
+  ],
 };
 
 export default SVG_COMPONENT_LIST;
