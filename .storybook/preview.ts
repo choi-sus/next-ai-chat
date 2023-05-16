09d41663799762172b5550b28cceb405aae98a65
choi-sus <choi-sus@naver.com>
2023-05-16 17:02:03 +0900

    [Chore] : storybook version update

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,7 @@
 import type { Preview } from '@storybook/react';
-import '../src/styles/globals.css';
 
 const preview: Preview = {
   parameters: {
-    nextjs: {
-      appDirectory: true,
-    },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
